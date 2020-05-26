1b5076e1949b58231f1ea5dd0609f4bb4cd41469
Rodrigo <clickmta123@gmail.com>
2020-05-25 21:17:19 -0400

    Filtros

--- proyecto/funciones.py
+++ proyecto/funciones.py
@@ -1,6 +1,24 @@
-def eliminar_ruido(frame):
+import numpy as np
+from scipy import fftpack
+from scipy.signal import medfilt
+def eliminar_ruido(frame,n,m):
+
+    freq=28
     # Implemente esta función
-    frame_limpio = frame
+    # ESTE CONDICION ES MOMENTANEA, ES PARA PROBAR CON FRAMES ESPECIFICOS
+    if n==m:
+        #Borra ruido impulsivo
+        frame=medfilt(frame, 5)
+        #Borra ruido periodico
+        frame_freq = fftpack.fftshift(fftpack.fft2(frame))
+        frame_freq[230-freq:260-freq,
+                   418:427] = 0
+        frame_freq[230+freq:260+freq,
+                   418:427] = 0
+        frame_limpio = np.abs(fftpack.ifft2(fftpack.ifftshift(frame_freq)))
+    else:
+        frame_limpio=frame
+
     return frame_limpio
 
 def transmisor(frame):

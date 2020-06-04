99515a48d224c176b5529a90d9f96628ddfb7076
Francisco <franciscoguionbajolevin@gmail.com>
2020-06-04 19:45:40 -0400

    return mas simple

--- proyecto/funciones.py
+++ proyecto/funciones.py
@@ -2,24 +2,20 @@ import numpy as np
 from scipy import fftpack
 from scipy.signal import medfilt
 
-def elimina_ruido_impulsivo(freq, frame): 
-    frame = medfilt(frame, 5)
+def elimina_ruido_impulsivo(frame): 
+    return medfilt(frame, 5)
     
-    return frame
-
 def elimina_ruido_periodico(freq, frame): 
     frame_freq = fftpack.fftshift(fftpack.fft2(frame))
     frame_freq[230-freq:260-freq, 418:427] = 0
     frame_freq[230+freq:260+freq, 418:427] = 0
-    frame_limpio = np.abs(fftpack.ifft2(fftpack.ifftshift(frame_freq)))
+    
+    return np.abs(fftpack.ifft2(fftpack.ifftshift(frame_freq)))
         
-    return frame_limpio
-
-
+    
 def eliminar_ruido(frame):
-
-    freq = 28
-    frame = elimina_ruido_impulsivo(freq, frame)
+    freq = 25
+    frame = elimina_ruido_impulsivo(frame)
     frame_limpio = elimina_ruido_periodico(freq, frame)
     
     return frame_limpio

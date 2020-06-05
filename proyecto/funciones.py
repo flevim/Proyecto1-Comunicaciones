00283dcf48803a356d6fb8b5c3015fcce72be7d3
Rodrigo <clickmta123@gmail.com>
2020-06-05 15:20:46 -0400

    Arbol de huffman

--- proyecto/funciones.py
+++ proyecto/funciones.py
@@ -3,6 +3,8 @@ from scipy import fftpack
 from scipy.signal import medfilt
 import matplotlib.pyplot as plt
 import cv2
+from collections import Counter
+import heapq
 
 def elimina_ruido_impulsivo(frame): 
     return medfilt(frame, 5)
@@ -36,11 +38,33 @@ def transforma_frame(frame):
 
 def cuantiza_frame(frame, frame_min, q):
     return frame_min + q/2 + np.floor((frame - frame_min) / q) * q
+
+def count(frame):
+    f = np.ravel(frame.astype(np.uint8))
+    return Counter(f).most_common()
+
+def huffman(frame):
+    dendograma = [[frequencia/len(frame), [simbolo, ""]] for simbolo, frequencia in count(frame)]
+    heapq.heapify(dendograma)
+    while len(dendograma) > 1:
+        lo = heapq.heappop(dendograma)
+        hi = heapq.heappop(dendograma)
+        for codigo in lo[1:]:
+            codigo[1] = '0' + codigo[1]
+        for codigo in hi[1:]:
+            codigo[1] = '1' + codigo[1]
+        heapq.heappush(dendograma, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    dendograma = sorted(heapq.heappop(dendograma)[1:])
+    dendograma = {simbolo : codigo for simbolo, codigo in dendograma} 
+    return dendograma
+    #display(dendograma)
+
     
 def transmisor(frame):
     frame_transformado = transforma_frame(frame)
     frame_cuantizado = cuantiza_frame(frame_transformado, frame_transformado.min(), q = 0.01)
     
+    h=huffman(frame_cuantizado)
     frame_comprimido = frame
     
     return frame_comprimido

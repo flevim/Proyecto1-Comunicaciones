a52d3c00059ef3e72b3307770ad65bd6f25ef1d1
Francisco <franciscoguionbajolevin@gmail.com>
2020-06-04 22:26:49 -0400

    Avances con transmisor

--- proyecto/funciones.py
+++ proyecto/funciones.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import fftpack
 from scipy.signal import medfilt
+import matplotlib.pyplot as plt
+import cv2
 
 def elimina_ruido_impulsivo(frame): 
     return medfilt(frame, 5)
@@ -19,17 +21,28 @@ def eliminar_ruido(frame):
     frame_limpio = elimina_ruido_periodico(freq, frame)
     
     return frame_limpio
+  
 
+def transforma_frame(frame): 
+    DCT2 = lambda g, norm='ortho': fftpack.dct( fftpack.dct(g, axis=0, norm=norm), axis=1, norm=norm)
+    frame_size = frame.shape
+    dct_matrix = np.zeros(shape = frame_size)
+
+    for i in range(0, frame_size[0], 8):
+        for j in range(0, frame_size[1], 8):
+            dct_matrix[i:(i+8),j:(j+8)] = DCT2(frame[i:(i+8),j:(j+8)])
+    
+    return dct_matrix
+
+def cuantiza_frame(frame, frame_min, q):
+    return frame_min + q/2 + np.floor((frame - frame_min) / q) * q
+    
 def transmisor(frame):
-    # Implemente los bloques de
-    #
-    # Transformación
-    #
-    # Cuantización
-    #
-    # Codificación de fuente
-    #
+    frame_transformado = transforma_frame(frame)
+    frame_cuantizado = cuantiza_frame(frame_transformado, frame_transformado.min(), q = 0.01)
+    
     frame_comprimido = frame
+    
     return frame_comprimido
 
 

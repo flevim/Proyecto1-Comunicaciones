dcf197544a54dd5b271360c44c2a2d43d51ac41b
Francisco <franciscoguionbajolevin@gmail.com>
2020-06-09 00:39:57 -0400

    Arreglos finales :(

--- proyecto/funciones.py
+++ proyecto/funciones.py
@@ -19,10 +19,10 @@ def elimina_ruido_periodico(freq, frame):
         
     
 def eliminar_ruido(frame):
+    #print(type(frame[0][0]))
     freq = 25
     frame = elimina_ruido_impulsivo(frame)
     frame_limpio = elimina_ruido_periodico(freq, frame)
-    
     return frame_limpio
   
 
@@ -33,20 +33,27 @@ def transforma_frame(frame, frame_size):
     for i in range(0, frame_size[0], 8):
         for j in range(0, frame_size[1], 8):
             dct_matrix[i:(i+8),j:(j+8)] = DCT2(frame[i:(i+8),j:(j+8)])
+            
     
     return dct_matrix
 
 
-def cuantiza_frame(frame, size, Q):
+def cuantiza_frame(frame, size, Q, peso_original):
+    nnz = np.zeros(frame.shape)
     for i in range(0, size[0], 8):
            for j in range(0, size[1], 8): 
                 frame[i:(i+8), j:(j+8)] = np.round(frame[i:(i+8), j:(j+8)] / Q)
+                nnz[i,j]=np.count_nonzero(frame[i:(i+8), j:(j+8)])
                 
+    peso_comprimido = np.sum(nnz)*8/1e+6
+    print("Peso comprimido: %0.2f MB" % peso_comprimido)  
+    print("Tasa de compresión: %0.2f MB/s" % (peso_comprimido / peso_original))
+    
     return frame
 
-def descuantiza_frame(frame, size):
+def recupera_frame(frame, size):
     IDCT2 = lambda G, norm='ortho': fftpack.idct( fftpack.idct(G, axis=0, norm=norm), axis=1, norm=norm)
-    im_dct = np.zeros(shape = size)
+    
     for i in range(0, size[0], 8):
            for j in range(0, size[1], 8): 
                 im_dct[i:(i+8), j:(j+8)] = IDCT2(frame[i:(i+8),j:(j+8)])
@@ -84,17 +91,20 @@ def codifica_frame(frame):
     for fila in frame:
         for i in range(len(fila)):
             tira_codificada += dendograma[fila[i].astype(np.uint8)]
-    #display(tira_codificada)
+    
     
     header.extend([dendograma, tira_codificada])
     
     return header
             
     
-    
 def transmisor(frame):
-
+    
     size = frame.shape
+    peso_original = np.prod(size) * 8 / 1e+6
+    
+    print("Peso original: %0.2f MB" % peso_original) 
+    
     Q = np.array([[3,2,2,3,5,8,10,12],
                   [2,2,3,4,5,11,11,13],
                   [3,2,3,5,8,11,13,11],
@@ -104,43 +114,35 @@ def transmisor(frame):
                   [9,12,15,17,20,23,23,19],
                   [14,17,18,19,21,19,20,19]])
     
-    print("Antes: ",frame)
     frame_transformado = transforma_frame(frame, size)
     
-    frame_cuantizado = cuantiza_frame(frame_transformado, size, Q)
-    #print(frame_cuantizado)
+    frame_cuantizado = cuantiza_frame(frame_transformado, size, Q, peso_original)
     
-   
-    frame_original = descuantiza_frame(frame_cuantizado, size)
-    print("Despues: ",frame_original)
-    #frame_codificado = codifica_frame(frame_cuantizado)
+    #frame_original = recupera_frame(frame_cuantizado, size)
     
-    frame_comprimido = frame_original
+    frame_codificado = codifica_frame(frame_cuantizado)
+    
+    frame_comprimido = frame_codificado
     
     return frame_comprimido
 
 
 def receptor(frame_comprimido):
-    '''
     codigo = ""
-    texto = []
+    frame_rec = []
+    
     dendograma = frame_comprimido[0]
     dendograma_inverso =  {codigo: simbolo for simbolo, codigo in dendograma.items()}
-    new_array = np.zeros((480,848), dtype=np.int8)
-    
-    #print(dendograma_inverso)
     
     for i in frame_comprimido[1]:
         codigo += i
         if codigo in dendograma_inverso:
-            texto.append(dendograma_inverso[codigo])
+            frame_rec.append(dendograma_inverso[codigo])
             codigo = ""
     
-       
-    arr = np.array(texto)
+    
+    arr = np.array(frame_rec)
     arr = np.array_split(arr, 480)
-    print(arr)
-    '''
     
     frame = frame_comprimido
     
